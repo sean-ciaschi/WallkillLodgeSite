d0c99fb0aa6834cbedd88103cf508a5bad34183e
sean.ciaschi <sean.ciaschi@goftx.com>
2017-06-02 12:34:03 -0400

    Added layout file

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+        @yield('head_styles')
+
+        @yield('head_scripts')
+    </head>
+
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
