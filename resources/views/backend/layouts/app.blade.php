e41a8220ecb40cb97dfd6a338f384f2c76d5b4ce
sean.ciaschi <sean.ciaschi@goftx.com>
2017-06-06 19:30:53 -0400

    Set up working post creation

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -69,6 +69,7 @@
             @include('backend.includes.footer')
         </div><!-- ./wrapper -->
 
+
         <!-- JavaScripts -->
         <script
                 src="https://code.jquery.com/jquery-3.2.1.min.js"
