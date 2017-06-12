76143d6f7e2b13cff480b21555e293096c6c00a0
sean.ciaschi <sean.ciaschi@goftx.com>
2017-06-12 19:27:43 -0400

    Formatted code

--- resources/views/frontend/calendar/calendar.blade.php
+++ resources/views/frontend/calendar/calendar.blade.php
@@ -54,20 +54,35 @@
 
                     if(calEvent.description != undefined)
                     {
-                        html += '<p class="event-desc"> <span class="event_child_txt">Description:</span> '+calEvent.description+'</p>';
+                        html += '<p class="event-desc">';
+                            html += '<span class="event_child_txt">Description:</span> '+ calEvent.description;
+                        html += '</p>';
                     }
 
-                    html += '<p class="start-time"> <span class="event_child_txt">Start Time:</span> '+moment(calEvent.start).format("MM/DD/YYYY hh:mma")+'</p>';
-                    html += '<p class="end-time"> <span class="event_child_txt">End Time:</span> '+moment(calEvent.end).format("MM/DD/YYYY hh:mma")+'</p>';
+                    html += '<p class="all-day">';
+                        html += '<span class="event_child_txt">All Day Event?:</span> '+(calEvent.allDay ? 'Yes' : 'No');
+                    html += '</p>';
+
+                    html += '<p class="start-time">';
+                        html += '<span class="event_child_txt">Start Time:</span> '+moment(calEvent.start).format("MM/DD/YYYY hh:mma");
+                    html += '</p>';
+
+                    html += '<p class="end-time">';
+                        html += '<span class="event_child_txt">End Time:</span> '+moment(calEvent.end).format("MM/DD/YYYY hh:mma");
+                    html += '</p>';
 
                     if(calEvent.location != undefined)
                     {
-                        html += '<p class="event_loc"> <span class="event_child_txt">Location:</span> '+calEvent.location+' (<a href="http://maps.google.com/maps?daddr='+calEvent.location+'" target="_blank">Directions</a>)</p>';
+                        html += '<p class="event_loc">';
+                            html += '<span class="event_child_txt">Location:</span> '+calEvent.location+' (<a href="http://maps.google.com/maps?daddr='+calEvent.location+'" target="_blank">Directions</a>)';
+                        html += '</p>';
                     }
 
                     if (calEvent.url)
                     {
-                        html += '<p class="open-gcal"> <span class="event_child_txt"><a href="'+calEvent.url+'" target="_blank">Copy Event to Google Calendar</a></span></p>';
+                        html += '<p class="open-gcal">';
+                            html += '<span class="event_child_txt"><a href="'+calEvent.url+'" target="_blank">Copy Event to Google Calendar</a></span>';
+                        html += '</p>';
                     }
 
                     selectors.modalTitle.innerHTML  = 'Event: ' + calEvent.title;
