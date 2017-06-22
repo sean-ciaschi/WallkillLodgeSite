2b3e9c80909a8bfb388a58389642f4b232e13c9c
sean.ciaschi <sean.ciaschi@goftx.com>
2017-06-22 16:34:10 -0400

    Added text-align:center to gallery images

--- resources/views/frontend/gallery/images.blade.php
+++ resources/views/frontend/gallery/images.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             @foreach($images as $image)
                 <div class="col-xs-12 col-md-4" style="text-align: center;">
-                    <a href="{{route('frontend.storage.album.images', ['albumId' => $albumId,'filename' => $image->image])}}" data-fancybox="gallery" class="col-md-12">
+                    <a href="{{route('frontend.storage.album.images', ['albumId' => $albumId,'filename' => $image->image])}}" data-fancybox="gallery" class="col-md-8">
                         <img class="inner-album-img img-thumbnail" src="{{route('frontend.storage.album.images', ['albumId' => $albumId,'filename' => $image->image])}}">
                     </a>
                 </div>
