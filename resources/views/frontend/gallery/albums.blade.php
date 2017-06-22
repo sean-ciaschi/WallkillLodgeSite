5a5b64ca802581c3136422c73a2ee30cc342ae7e
sean.ciaschi <sean.ciaschi@goftx.com>
2017-06-22 12:48:29 -0400

    Setting up gallery on frontend

--- resources/views/frontend/gallery/albums.blade.php
+++ resources/views/frontend/gallery/albums.blade.php
@@ -4,9 +4,8 @@
     <div class="container">
         <div class="row">
             @foreach($albums as $album)
-                {{$album->id}}
-                <div class="well col-md-5">
-                    <img class="album-img" src="{{route('frontend.storage.album', ['filename' => $album->cover_image])}}">
+                <div class="well col-xs-12 col-md-5">
+                    <img class="album-img img-thumbnail"  src="{{route('frontend.storage.album', ['filename' => $album->cover_image])}}">
                     <h2>{{$album->name}}</h2>
                     <p>{{str_limit($album->description, 150)}}</p>
                     <a href="{{route('frontend.gallery.images', ['albumId' => $album->id])}}"><div class="btn btn-success pull-right">View Album</div></a>
