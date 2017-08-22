1cb17b217af117317bae4f8e9bd22d0d3a8d743b
sean.ciaschi <sean.ciaschi@goftx.com>
2017-08-22 19:30:17 -0400

    Added migrations for events

---
+++ database/migrations/2017_08_22_232516_create_data_events_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDataEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_events', function(Blueprint $table)
+        {
+            $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->text('description');
+            $table->string('location');
+            $table->string('date');
+            $table->decimal('price', 10, 2);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
