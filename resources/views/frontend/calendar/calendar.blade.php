481902b370a44ab17092da175439635ca7095883
sean.ciaschi <sean.ciaschi@goftx.com>
2017-09-05 18:25:28 -0400

    Fixed events on mobile

--- resources/views/frontend/calendar/calendar.blade.php
+++ resources/views/frontend/calendar/calendar.blade.php
@@ -35,6 +35,7 @@
 
 @section('after-scripts')
     @mobile()
+        <script src="{{asset('build/plugins/fullcalendar/gcal.min.js')}}"></script>
         <script type='text/javascript' src="assets/js/calendar.mobile.js"></script>
     @elsemobile
         <script src="{{asset('build/plugins/fullcalendar/gcal.min.js')}}"></script>
