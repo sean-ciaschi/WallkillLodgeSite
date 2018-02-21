bfb80777336a493f07ef86a81e8ac59889a86d0b
sean.ciaschi <sean.ciaschi@goftx.com>
2018-02-21 16:49:24 -0500

    Minor fixes

--- routes/Backend/Blog.php
+++ routes/Backend/Blog.php
@@ -6,7 +6,7 @@
 Route::group(['namespace' => 'Blog'], function () {
     Route::get('blog', 'AdminBlogController@index')->name('blog');
     Route::get('blog/blog-create', 'AdminBlogController@create')->name('blog.create');
-    Route::delete('blog/delete-post/{id}', 'AdminBlogController@destroy')->name('blog.delete-post');
+    Route::post('blog/delete-post/{id}', 'AdminBlogController@destroy')->name('blog.delete-post');
     Route::get('blog/edit-post/{id}', 'AdminBlogController@update')->name('blog.edit-post');
 
     Route::post('blog/create-post', 'AdminBlogController@createPost')->name('blog.create-post');
